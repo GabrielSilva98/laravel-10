39e5652030b09fb6147e0bb2d3abcd284f59bce4
GabrielSilva98 <gabriel8198@gmail.com>
2023-11-08 20:21:22 -0300

    Adicionando rota create support

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\SupportController;
 use App\Http\Controllers\Site\SiteController;
 
+
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
 Route::get('/contato', [SiteController::class, 'contact']);
